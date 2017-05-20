d93ddc0ed9f8829c892ef437c778f8a01df8344f
Stephane LOPES-NEVES <stephane.lopes@metigate.com>
2017-05-20 21:25:44 +0200

    (test) Iterative search find element

---
+++ src/test/java/fr/slopes/karatechop/search/IterativeSearchTest.java
@@ -0,0 +1,20 @@
+package fr.slopes.karatechop.search;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class IterativeSearchTest {
+    @Test
+    public void should_return_index_of_element_if_element_is_in_ordered_array () {
+        //Given
+        BinarySearch binarySearch = new IterativeSearch();
+        int[] elements = {1, 2, 3, 4};
+
+        //When
+        Integer result = binarySearch.search(2, elements);
+
+        //Then
+        Assertions.assertThat(result).isEqualTo(2);
+    }
+
+}
