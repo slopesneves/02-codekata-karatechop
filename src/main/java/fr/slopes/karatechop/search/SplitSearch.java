1a0fe1d1d86f1dc3156850b8b57ccaf7d6c59d84
Stephane LOPES-NEVES <stephane.lopes@metigate.com>
2017-05-26 23:36:37 +0200

    (clean) Iterative way instead of recursive

--- src/main/java/fr/slopes/karatechop/search/SplitSearch.java
+++ src/main/java/fr/slopes/karatechop/search/SplitSearch.java
@@ -13,16 +13,19 @@ public class SplitSearch implements BinarySearch {
     }
 
     private int search(int minIndex, int maxIndex, int elementToFind, int[] elements) {
-        int middleIndex = (maxIndex - minIndex) / 2 + minIndex;
-        int evaluateElement = elements[middleIndex];
-        if (evaluateElement == elementToFind) {
-            return middleIndex;
-        }else if(minIndex == maxIndex) {
-            return ELEMENT_NOT_FOUND;
-        }else if(evaluateElement < elementToFind) {
-            return search(middleIndex + 1, maxIndex, elementToFind, elements);
-        }else {
-            return search(minIndex, middleIndex - 1, elementToFind, elements);
+        while (minIndex != maxIndex) {
+            int middleIndex = (maxIndex - minIndex) / 2 + minIndex;
+            int evaluateElement = elements[middleIndex];
+            if (evaluateElement == elementToFind) {
+                return middleIndex;
+            }else if(evaluateElement < elementToFind) {
+                minIndex = middleIndex + 1;
+            }else {
+                maxIndex = middleIndex - 1;
+            }
         }
+
+        return ELEMENT_NOT_FOUND;
+
     }
 }
