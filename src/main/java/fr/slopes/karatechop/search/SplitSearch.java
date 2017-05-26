ce8eb87dcf32ae297d9d033e7a5f22d91522a0ae
Stephane LOPES-NEVES <stephane.lopes@metigate.com>
2017-05-26 23:13:52 +0200

    (feat) Split search implementation

--- src/main/java/fr/slopes/karatechop/search/SplitSearch.java
+++ src/main/java/fr/slopes/karatechop/search/SplitSearch.java
@@ -5,6 +5,25 @@ import sun.security.util.PendingException;
 public class SplitSearch implements BinarySearch {
     @Override
     public int search(int elementToFind, int[] elements) {
-        throw new PendingException();
+        if (elements.length == 0) {
+            return -1;
+        }
+        return search(0, elements.length - 1, elementToFind, elements);
+    }
+
+    private int search(int minIndex, int maxIndex, int elementToFind, int[] elements) {
+        int middleIndex = (maxIndex - minIndex) / 2 + minIndex;
+        int evaluateElement = elements[middleIndex];
+        if (evaluateElement == elementToFind) {
+            return middleIndex;
+        }else if(minIndex == maxIndex) {
+            return -1;
+        }else if(evaluateElement < elementToFind) {
+            return search(middleIndex + 1, maxIndex, elementToFind, elements);
+        }else if(evaluateElement > elementToFind) {
+            return search(minIndex, middleIndex - 1, elementToFind, elements);
+        }else{
+            throw new PendingException();
+        }
     }
 }
