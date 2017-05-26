e283c444d9ee20179c532db8bfe18e24c28b12a3
Stephane LOPES-NEVES <stephane.lopes@metigate.com>
2017-05-26 23:39:24 +0200

    (test) Find first element in array

--- src/test/java/fr/slopes/karatechop/search/BinarySearchTest.java
+++ src/test/java/fr/slopes/karatechop/search/BinarySearchTest.java
@@ -31,6 +31,18 @@ public class BinarySearchTest {
     }
 
     @Test
+    public void should_return_index_of_element_if_element_is_first_in_ordered_array () {
+        //Given
+        int[] elements = {1, 2, 3, 4};
+
+        //When
+        Integer result = binarySearch.search(1, elements);
+
+        //Then
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
     public void should_return_negative_one_value_if_search_on_empty_array () {
         //Given
         int[] elements = {};
